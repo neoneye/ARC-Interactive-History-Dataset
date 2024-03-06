cd99dc652026143874fe6d8b76b6ec9cdb996d95
Simon Strandgaard <neoneye@gmail.com>
2024-03-06 09:22:28 +0100

    bail out if there are no changes to the repo

--- history_files/full_sync.py
+++ history_files/full_sync.py
@@ -1,7 +1,18 @@
+import subprocess
 import os
 
 # run import_json_job
 os.system('python3 import_json_job.py')
+
+# Run git status --porcelain and capture its output
+result = subprocess.run(['git', 'status', '--porcelain'], stdout=subprocess.PIPE, text=True)
+
+# Check if the output is empty
+if result.stdout == "":
+    print("No changes to commit")
+    exit()
+
+print("There are changes to commit")
 
 # regenerate directory_content.json
 os.system('python3 directory_content_generate.py')
